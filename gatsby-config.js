132f177429cfc453c725d3162fbdfc526faf9955
Giuliano Mozzillo <mozzillation@me.com>
2020-01-31 14:41:41 +0100

    nprogress added

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `RMF — Rural Mobility Framework`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `@mozzillation`,
   },
   pathPrefix: "/rural-mobility",
   plugins: [
@@ -24,14 +24,28 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `docs`,
-        path: `./src/doc`,
+        path: `${__dirname}/src/doc`,
       },
     },
+    {
+    resolve: `gatsby-transformer-remark`,
+    options: {
+      plugins: [
+        {
+          resolve: `gatsby-remark-images`,
+          options: {
+            path: `${__dirname}/src/data/`,
+            maxWidth: 590,
+          },
+        },
+      ],
+    },
+  },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `data`,
-        path: `./src/data/`,
+        path: `${__dirname}/src/data/`,
       },
     },
     {
@@ -40,10 +54,10 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#FFF`,
+        theme_color: `#00B868`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `${__dirname}/src/images/rmf_icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
